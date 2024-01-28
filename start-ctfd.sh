10f63a2fe7bff11e15aa847f069922d7b2c53fad
Michael "Santosti" Sousa <69021884+michaelsantosti@users.noreply.github.com>
2024-01-28 16:29:04 -0300

    🏗️ rebuild setup.sh
    Yeah, more cleaner now

--- start-ctfd.sh
+++ start-ctfd.sh
@@ -13,48 +13,64 @@ cat <<EOF
    Date............ 05/01/2024              Version....... V1.0
    By.............. Michael Santosti        Target........ CTFd
  ==============================================================================
-
 EOF
 
 #Set Working Directory
 SCRIPT_WORK="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-# Bind a Work Directory
+#Bind a Work Directory
 echo Working Directory: ${SCRIPT_WORK}
+cd /tmp
 
-# Build Stacks
+#Removing old CTFd
+sudo rm -rf CTFd
+
+#Download CTFd
+sudo git clone --single-branch https://github.com/CTFd/CTFd.git CTFd
+
+#Download Themes
+sudo git clone https://github.com/0xdevsachin/CTFD-crimson-theme.git CTFd/CTFd/themes/crimson
+sudo git clone https://github.com/hmrserver/CTFd-theme-pixo.git CTFd/CTFd/themes/pixo
+sudo git clone https://github.com/0xdevsachin/CTFD-odin-theme.git CTFd/CTFd/themes/odin
+sudo git clone https://github.com/iver-ics/CTFd-xmas-theme.git CTFd/CTFd/themes/xmas
+sudo git clone https://github.com/chainflag/ctfd-neon-theme.git CTFd/CTFd/themes/neon
+sudo git clone https://github.com/AaronVigal/nullify-ctfd-theme.git && sudo mv ./nullify-ctfd-theme/nullify/ CTFd/CTFd/themes/ && sudo rm -rf nullify-ctfd-theme/
+
+#Install curl on build (enable healthcheck)
+sed -i '34i\        curl \ \' CTFd/Dockerfile
+
+#Build Stacks
 echo ''
 echo '------> Building 1TDCOA Stack'
 echo ''
 
-cd ${SCRIPT_WORK}/CTFd/1tdcoa/ctfd/
-sudo chmod +x setup.sh
-sudo /bin/bash setup.sh
+sudo rm -r ${SCRIPT_WORK}/CTFd/1tdcoa/ctfd/CTFd
+sudo cp -r CTFd ${SCRIPT_WORK}/CTFd/1tdcoa/ctfd/
 
 echo ''
 echo '------> Building 1TDCOR Stack'
 echo ''
 
-cd ${SCRIPT_WORK}/CTFd/1tdcor/ctfd/
-sudo chmod +x setup.sh
-sudo /bin/bash setup.sh
+sudo rm -r ${SCRIPT_WORK}/CTFd/1tdcor/ctfd/CTFd
+sudo cp -r CTFd ${SCRIPT_WORK}/CTFd/1tdcor/ctfd/
 
 echo ''
 echo '------> Building 2TDCOA Stack'
 echo ''
 
-cd ${SCRIPT_WORK}/CTFd/2tdcoa/ctfd/
-sudo chmod +x setup.sh
-sudo /bin/bash setup.sh
+sudo rm -r ${SCRIPT_WORK}/CTFd/2tdcoa/ctfd/CTFd
+sudo cp -r CTFd ${SCRIPT_WORK}/CTFd/2tdcoa/ctfd/
 
 echo ''
 echo '------> Building 2TDCOR Stack'
 echo ''
 
-cd ${SCRIPT_WORK}/CTFd/2tdcor/ctfd/
-sudo chmod +x setup.sh
-sudo /bin/bash setup.sh
+sudo rm -r ${SCRIPT_WORK}/CTFd/2tdcor/ctfd/CTFd
+sudo cp -r CTFd ${SCRIPT_WORK}/CTFd/2tdcor/ctfd/
 
+#Deleting Temp Files
+
+sudo rm -r CTFd
 
 #Initializing Stacks
 echo ''
